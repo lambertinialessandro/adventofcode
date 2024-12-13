b6ed2e4f72efce34d783ecbce49278afba7acc86
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2024-12-13 22:23:15 +0100

    added solution to day 13

---
+++ 2024/13/main_part_1.py
@@ -0,0 +1,44 @@
+
+'''
+Your puzzle answer was 31897.
+
+The first half of this puzzle is complete! It provides one gold star: *
+'''
+
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from utils.read_file import read_file
+from utils.test_solution import test_solution
+
+import re
+
+@test_solution(name="DAY 13 PART 1", runs=100)
+def main():
+    print("\n### DAY 13 PART 1 ###")
+
+    content = read_file(os.path.join(os.path.dirname(__file__),"input.txt"))
+    if not content: return
+
+    buttons_a = re.findall("A: X\+(\d+), Y\+(\d+)", content)
+    buttons_b = re.findall("B: X\+(\d+), Y\+(\d+)", content)
+    prizes = re.findall("Prize: X=(\d+), Y=(\d+)", content)
+
+    total_prizes = 0
+    for button_a, button_b, prize in [*zip(buttons_a, buttons_b, prizes)]:
+        [bax, bay] = [*map(int, button_a)]
+        [bbx, bby] = [*map(int, button_b)]
+        [px, py] = [*map(int, prize)]
+
+        # x b_a_x + y b_b_x = p_x
+        # x b_a_y + y b_b_y = p_y
+        y = (py*bax - px*bay)/(bby*bax - bbx*bay)
+        x = (px - y*bbx)/bax
+
+        if x % 1 == 0 and y % 1 == 0:
+            total_prizes += 3*x + y
+        
+    print(f"\n# SOLUTION: tokens spend to win all possible prizes: {int(total_prizes)}\n")
+
+if __name__ == "__main__":
+    main()
