9dc26363a3fa92209fd35cbeb7a28f6ba37db835
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2024-12-20 00:00:13 +0100

    added sub, lt  and hash to enum

--- utils/enum.py
+++ utils/enum.py
@@ -10,9 +10,20 @@ class SmartEnum(Enum):
     def __str__(self):
         return self.name
     
+    def __sub__(self, other):
+        return self.__add__(other)
+    
     def __add__(self, other):
         if isinstance(other, int):
             num_elements = len(self.__class__)
             next_value = (self.value + other) % num_elements
             return [*self.__class__][next_value]
         return NotImplemented
+    
+    def __lt__(self, other):
+        if isinstance(other, SmartEnum):
+            return self.value < other.value
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(self.value)
