5d09bed9ac264966f5d58fbea72a71509e95a2d6
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2025-10-27 19:37:21 +0100

    uploaded requirements

---
+++ requirements.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import re
+import time
+import requests
+from enum import Enum
+from contextlib import redirect_stdout
+from io import StringIO
+from dotenv import load_dotenv
